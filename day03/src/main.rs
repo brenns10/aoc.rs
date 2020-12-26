1da33682cada9ece05cb8a9a92353eeb7d3f642a
Stephen Brennan <stephen@brennan.io>
2020-12-25 16:45:19 -0800

    day03: task 1

---
+++ day03/src/main.rs
@@ -0,0 +1,52 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::vec::Vec;
+use std::result::Result;
+
+fn read_map(filename: &str) -> Result<Vec<Vec<char>>, String> {
+    let file = File::open(filename).map_err(|e| e.to_string())?;
+    let reader = BufReader::new(file);
+    let mut cols: usize = 0;
+    let mut first = true;
+    let mut rows: Vec<Vec<char>> = Vec::new();
+
+    for (line_no, line) in reader.lines().enumerate() {
+        let line = line.map_err(|e| e.to_string())?;
+        let chars: Vec<char> = line.chars().collect();
+        for c in &chars {
+            if *c != '#' && *c != '.' {
+                return Err(format!("Invalid character {} at line {}", c, line_no + 1));
+            }
+        }
+        if first {
+            cols = chars.len();
+            first = false;
+        }
+        if chars.len() != cols {
+            return Err(format!("At line {}, expected {} chars but found {}",
+                               line_no + 1, cols, chars.len()));
+        }
+        rows.push(chars);
+    }
+    Ok(rows)
+}
+
+fn count_trees(map: &Vec<Vec<char>>, slope: usize) -> usize {
+    let mut col = 0;
+    let mut trees = 0;
+    let cols = map[0].len();
+    for row in map {
+        if row[col] == '#' {
+            trees += 1;
+        }
+        col = (col + slope) % cols;
+    }
+    trees
+}
+
+fn main() {
+    let map = read_map("input.txt").unwrap();
+    let slope = 3;
+    println!("Trees in slope {}: {}", slope, count_trees(&map, slope));
+}
