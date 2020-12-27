1f8b7a177d90de4f6c6fd4c67f6efb584f1411c8
Stephen Brennan <stephen@brennan.io>
2020-12-26 18:11:28 -0800

    day04: task 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,48 +1,105 @@
+use regex::Captures;
+use regex::Regex;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
 use std::vec::Vec;
 use std::result::Result;
 
-fn count_valid_passports(filename: &str, reqd: &Vec<String>) -> Result<usize, String> {
+struct Policy {
+    expr: Regex,
+    valid: Option<Box<dyn Fn(&Captures) -> Result<bool, String>>>,
+}
+
+fn policy_match(value: &str, policy: &Policy) -> Result<bool, String> {
+    match policy.expr.captures(value) {
+        None => Ok(false),
+        Some(c) => match &policy.valid {
+            None => Ok(true),
+            Some(f) => f.as_ref()(&c),
+        }
+    }
+}
+
+fn minmax(min: isize, max: isize) -> Box<dyn Fn(&Captures) -> Result<bool, String>> {
+    Box::new(move |c: &Captures| {
+        let m = c.get(1).ok_or("missing match!")?;
+        let res: isize = m.as_str().parse().map_err(|_| String::from("couldnt parse"))?;
+        Ok(res >= min && res <= max)
+    })
+}
+
+fn minmax_hgt(c: &Captures) -> Result<bool, String> {
+    let m = c.get(1).ok_or("missing match!")?;
+    let u = c.get(2).ok_or("missing unit!")?;
+    let res: isize = m.as_str().parse().map_err(|_| String::from("couldnt parse"))?;
+    if u.as_str() == "cm" {
+        Ok(res >= 150 && res <= 193)
+    } else {
+        Ok(res >= 59 && res <= 76)
+    }
+}
+
+fn count_valid_passports(filename: &str, reqd: &Vec<Policy>) -> Result<usize, String> {
     let mut valid: usize = 0;
     let mut count: usize = 0;
     let file = File::open(filename).map_err(|e| e.to_string())?;
     let reader = BufReader::new(file);
 
-    let mut reqd_cp = reqd.to_vec();
+    let mut seen: Vec<bool> = vec![false; reqd.len()];
+    let mut counts = vec![0; reqd.len()];
     for line in reader.lines() {
         let line = line.map_err(|e| e.to_string())?;
 
         if line.len() == 0 {
             count += 1;
-            if reqd_cp.len() == 0 {
+            if seen.iter().all(|x| *x) {
                 valid += 1;
             } else {
                 // invalid, I guess do nothing?
             }
-            reqd_cp = reqd.to_vec();
+            seen = vec![false; reqd.len()];
         }
         for token in line.split_whitespace() {
-            let mut iter = token.splitn(2, ':');
-            let key = iter.next().ok_or("some error")?;
-            iter.next().ok_or("missing : in token")?;
-            reqd_cp.iter().position(|item| item == key).map(|i| reqd_cp.remove(i));
+            for (i, candidate) in reqd.iter().enumerate() {
+                if policy_match(token, candidate)? {
+                    seen[i] = true;
+                    counts[i] += 1;
+                    break;
+                }
+            }
         }
     }
-    if reqd_cp.len() == 0 {
+    if seen.iter().all(|x| *x) {
         valid += 1;
     }
     count += 1;
 
     println!("Found {} passports, {} valid", count, valid);
+    println!("Rule matches: {:?}", &counts);
     Ok(valid)
 }
 
 fn main() {
     println!("Hello, world!");
-    let mv: Vec<String> = vec![
-        "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"
-    ].iter().map(|s| String::from(*s)).collect();
+    let mv: Vec<Policy> = vec![
+        Policy{expr: Regex::new("^byr:\\S+$").unwrap(), valid: None},
+        Policy{expr: Regex::new("^iyr:\\S+$").unwrap(), valid: None},
+        Policy{expr: Regex::new("^eyr:\\S+$").unwrap(), valid: None},
+        Policy{expr: Regex::new("^hgt:\\S+$").unwrap(), valid: None},
+        Policy{expr: Regex::new("^hcl:\\S+$").unwrap(), valid: None},
+        Policy{expr: Regex::new("^ecl:\\S+$").unwrap(), valid: None},
+        Policy{expr: Regex::new("^pid:\\S+$").unwrap(), valid: None}
+    ];
     count_valid_passports("input.txt", &mv).unwrap();
+    let part2policies: Vec<Policy> = vec![
+        Policy{expr: Regex::new("^byr:(\\d{4})$").unwrap(), valid: Some(minmax(1920, 2002))},
+        Policy{expr: Regex::new("^iyr:(\\d{4})$").unwrap(), valid: Some(minmax(2010, 2020))},
+        Policy{expr: Regex::new("^eyr:(\\d{4})$").unwrap(), valid: Some(minmax(2020, 2030))},
+        Policy{expr: Regex::new("^hgt:(\\d+)(cm|in)$").unwrap(), valid: Some(Box::new(minmax_hgt))},
+        Policy{expr: Regex::new("^hcl:#[0-9a-f]{6}$").unwrap(), valid: None},
+        Policy{expr: Regex::new("^ecl:(amb|blu|brn|gry|grn|hzl|oth)$").unwrap(), valid: None},
+        Policy{expr: Regex::new("^pid:\\d{9}$").unwrap(), valid: None},
+    ];
+    count_valid_passports("input.txt", &part2policies).unwrap();
 }
