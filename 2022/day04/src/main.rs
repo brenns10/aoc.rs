26b97403013742b8964294c131a0992d91b46983
Stephen Brennan <stephen@brennan.io>
2022-12-05 19:57:56 -0800

    2022: day04: part 2

--- 2022/day04/src/main.rs
+++ 2022/day04/src/main.rs
@@ -2,7 +2,8 @@ use std::fs::File;
 use std::io::{self, BufRead};
 
 fn main() {
-    let mut count = 0;
+    let mut count_full = 0;
+    let mut count_part = 0;
     for line in io::BufReader::new(File::open("input.txt").unwrap()).lines() {
         let line = line.unwrap();
         let fields: Vec<&str> = line.split(['-', ',']).collect();
@@ -10,8 +11,12 @@ fn main() {
         let fields: Vec<u32> = fields.iter().map(|s| u32::from_str_radix(s, 10).unwrap()).collect();
         if ((fields[0] <= fields[2]) && (fields[3] <= fields[1])) ||
            ((fields[2] <= fields[0]) && (fields[1] <= fields[3])) {
-            count += 1;
+            count_full += 1;
+        }
+        if (fields[1] >= fields[2]) && (fields[0] <= fields[3]) {
+            count_part += 1;
         }
     }
-    println!("Pairs fully contained: {}", count);
+    println!("Pairs fully contained: {}", count_full);
+    println!("Pairs partially contained: {}", count_part);
 }
