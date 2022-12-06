d5aac07a1d052398538ad4640f8207b4f9926532
Stephen Brennan <stephen@brennan.io>
2022-12-06 00:24:28 -0800

    2022: day06: part 2
    For fun, I tried to make find_marker() more like something I would want
    from a library: rather than opening the input file directly, it now
    accepts an iterator of u8, and this means I can actually read the file
    in and use it twice as an iterator. Interesting, I guess.

--- 2022/day06/src/main.rs
+++ 2022/day06/src/main.rs
@@ -1,16 +1,18 @@
 use std::fs::File;
 use std::io::{self, Read, Result};
+use std::iter;
 
-fn find_marker() -> Result<u32> {
-    let mut buf = [0 as u8; 4];
-    let br = io::BufReader::new(File::open("input.txt")?);
-    for (i, c) in br.bytes().enumerate() {
-        let c = c?;
-        buf[i % 4] = c;
+/// Find the first occurrence of a sliding window of `size` bytes which are
+/// all different from each other in `br`.
+fn find_marker<'a, R>(br: R, size: usize) -> Result<u32>
+    where R: Iterator<Item = &'a u8>
+{
+    let mut buf = Vec::from_iter(iter::repeat(0 as u8).take(size));
+    for (i, c) in br.enumerate() {
+        buf[i % size] = *c;
         let mut sbuf = buf.clone();
         sbuf.sort();
-        println!("{} {} {:?} {:?}", i, c, buf, sbuf);
-        if i >= 3 && (0..sbuf.len() - 1).all(|i| sbuf[i] != sbuf[i + 1]) {
+        if i >= size - 1 && (0..sbuf.len() - 1).all(|i| sbuf[i] != sbuf[i + 1]) {
             return Ok((i + 1) as u32);
         }
     }
@@ -18,6 +20,10 @@ fn find_marker() -> Result<u32> {
 }
 
 fn main() {
-    let m = find_marker().unwrap();
-    println!("Characters to read: {}", m);
+    let file = File::open("input.txt").unwrap();
+    let buf = file.bytes().collect::<Result<Vec<u8>>>().unwrap();
+    let m = find_marker(buf.iter(), 4).unwrap();
+    println!("Characters to read, window 4: {}", m);
+    let m = find_marker(buf.iter(), 14).unwrap();
+    println!("Characters to read, window 14: {}", m);
 }
