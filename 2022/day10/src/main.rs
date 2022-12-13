2258814bc749b50c2b7cd26d25c59294d1c2c09d
Stephen Brennan <stephen@brennan.io>
2022-12-13 13:37:49 -0800

    2022: day10: part 2

--- 2022/day10/src/main.rs
+++ 2022/day10/src/main.rs
@@ -7,7 +7,7 @@ use std::env;
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 fn do_cycle(cycle: &mut usize, x: &isize, sigstrength: &mut isize) {
-    println!("During cycle {}, x={}", cycle, x);
+    //println!("During cycle {}, x={}", cycle, x);
     if *cycle % 40 == 20 {
         println!("Strength component, cycle {}: {}", cycle, (*cycle as isize) * *x);
         *sigstrength += (*cycle as isize) * *x;
@@ -15,7 +15,20 @@ fn do_cycle(cycle: &mut usize, x: &isize, sigstrength: &mut isize) {
     *cycle += 1;
 }
 
-fn read_instructions(filename: &str) -> MyResult<()> {
+fn do_cycle_crt(cycle: &mut usize, x: &isize, _: &mut isize) {
+    let cur_pixel = ((*cycle - 1) as isize) % 40;
+    if cur_pixel == 0 && *cycle != 1 {
+        print!("\n");
+    }
+    if *x - 1 <= cur_pixel && cur_pixel <= *x + 1 {
+        print!("#");
+    } else {
+        print!(".");
+    }
+    *cycle += 1;
+}
+
+fn read_instructions(filename: &str, func: fn(&mut usize, &isize, &mut isize)) -> MyResult<()> {
     let file = File::open(filename)?;
     let reader = io::BufReader::new(file);
 
@@ -27,17 +40,17 @@ fn read_instructions(filename: &str) -> MyResult<()> {
         let line = line?;
         let tokens: Vec<&str> = line.split_whitespace().collect();
         if tokens.len() == 1 && tokens[0] == "noop" {
-            do_cycle(&mut cycle, &x, &mut sigstrength);
+            func(&mut cycle, &x, &mut sigstrength);
         } else if tokens.len() == 2 && tokens[0] == "addx" {
             let increment = isize::from_str_radix(tokens[1], 10)?;
-            do_cycle(&mut cycle, &x, &mut sigstrength);
-            do_cycle(&mut cycle, &x, &mut sigstrength);
+            func(&mut cycle, &x, &mut sigstrength);
+            func(&mut cycle, &x, &mut sigstrength);
             x += increment;
         } else {
             return Err(format!("Bad input line: {}", line).into());
         }
     }
-    println!("Signal strength: {}", sigstrength);
+    println!("\nSignal strength: {}", sigstrength);
     Ok(())
 }
 
@@ -47,5 +60,8 @@ fn main() {
     if args.len() >= 2 {
         filename = &args[1];
     }
-    read_instructions(filename).unwrap();
+    println!("First, computing the signal strength:");
+    read_instructions(filename, do_cycle).unwrap();
+    println!("Second, printing the CRT:");
+    read_instructions(filename, do_cycle_crt).unwrap();
 }
