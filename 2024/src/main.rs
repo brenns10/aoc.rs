0ce9ef0f24ca2de1f1b0b8b111f02586390f7fc9
Stephen Brennan <stephen@brennan.io>
2024-12-02 23:30:21 -0800

    2024: import runner and utils
    Signed-off-by: Stephen Brennan <stephen@brennan.io>

---
+++ 2024/src/main.rs
@@ -0,0 +1,93 @@
+use std::env;
+use std::iter::Iterator;
+use std::time::Instant;
+
+use util::RunResult;
+
+mod arr;
+mod util;
+
+
+type Runner = fn(&str) -> RunResult;
+struct TestCase(Runner, Option<isize>, Option<isize>, Option<isize>, Option<isize>);
+
+const DAYS: &[TestCase] = &[
+];
+
+fn run_one(case: &TestCase, fln: &str, expected: Option<(Option<isize>, Option<isize>)>) -> bool {
+    let start = Instant::now();
+    let res = case.0(fln);
+    let elapsed = start.elapsed();
+    match res {
+        Err(e) => {
+            println!("❌ - {}\n{}.{:03}s", e, elapsed.as_secs(), elapsed.subsec_millis());
+            false
+        }
+        Ok((p1, p2)) => {
+            if let Some((e1, e2)) = expected {
+                if p1 == e1 && p2 == e2 {
+                    println!("✅ in {}.{:03}s", elapsed.as_secs(), elapsed.subsec_millis());
+                    true
+                } else {
+                    println!("❌ - expected {:?}, {:?}, got {:?}, {:?}\n{}.{:03}s", e1, e2, p1, p2, elapsed.as_secs(), elapsed.subsec_millis());
+                    false
+                }
+            } else {
+                println!("✅ in {}.{:03}s", elapsed.as_secs(), elapsed.subsec_millis());
+                true
+            }
+        }
+    }
+}
+
+fn do_run(dayno: u32, case: &TestCase, example: &str, fln: &str) -> usize {
+    println!("==> Day {} <==", dayno);
+    let mut fails = 0;
+    println!(" -> Example:");
+    if !run_one(case, example, Some((case.1, case.2))) { fails += 1 }
+    println!(" -> Puzzle:");
+    if !run_one(case, fln, Some((case.3, case.4))) { fails += 1 }
+    println!();
+    fails
+}
+
+fn default_input(dayno: u32) -> String {
+    format!("src/day{:02}/input.txt", dayno)
+}
+
+fn example_input(dayno: u32) -> String {
+    format!("src/day{:02}/example.txt", dayno)
+}
+
+fn run_all() -> usize {
+    let mut fails = 0;
+    let start = Instant::now();
+    for (i, runner) in DAYS.iter().enumerate() {
+        let i = i as u32 + 1;
+        fails += do_run(i, &runner, &example_input(i), &default_input(i));
+    }
+    let elapsed = start.elapsed();
+    println!("All tests completed.");
+    if fails == 0 {
+        println!("✅ in {}.{:03}s", elapsed.as_secs(), elapsed.subsec_millis());
+    } else {
+        println!("❌ {} failed in {}.{:03}s", fails, elapsed.as_secs(), elapsed.subsec_millis());
+    }
+    fails
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 || args[1] == "all" {
+        run_all();
+    } else {
+        let day = u32::from_str_radix(&args[1], 10).unwrap();
+        let case = &DAYS[day as usize - 1];
+
+        if args.len() >= 3 {
+            run_one(case, &args[2], None);
+        } else {
+            run_one(case, &default_input(day), Some((case.3, case.4)));
+        }
+    }
+}
---
+++ 2024/src/util.rs
@@ -0,0 +1,22 @@
+use std::{error::Error, num::ParseIntError};
+use std::str::FromStr;
+
+pub type MyResult<T> = Result<T, Box<dyn Error>>;
+pub type RunResult = MyResult<(Option<isize>, Option<isize>)>;
+
+#[allow(dead_code)]
+pub fn return_part1(v: isize) -> RunResult { Ok((Some(v), None)) }
+#[allow(dead_code)]
+pub fn return_part1and2(v: isize, w: isize) -> RunResult { Ok((Some(v), Some(w))) }
+
+#[allow(dead_code)]
+pub fn read_ints<T: FromStr>(s: &str) -> Result<Vec<T>, ParseIntError>
+where ParseIntError: From<<T as FromStr>::Err> {
+    let mut vec = Vec::new();
+
+    for num in s.split_ascii_whitespace() {
+        vec.push(T::from_str(num)?);
+    }
+
+    Ok(vec)
+}
